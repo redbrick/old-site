a1196c08376c114c0e7348fd66f3e2dc21e1bab0
Andrew Martin <werdz@redbrick.dcu.ie>
2010-03-04 23:13:23 +0000

    Playing with themes

---
+++ rbsite/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from rbsite.blog.feeds import LatestEntries
+import rbsite.settings
+
+
+feeds = {
+	'latest': LatestEntries,
+}
+
+# Auto discover admin stuff
+admin.autodiscover()
+
+urlpatterns = patterns('',
+	# news posts.
+	(r'^home/', include('rbsite.blog.urls')),
+	
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+
+	# Comments
+    (r'^comments/', include('django.contrib.comments.urls')),
+    
+    # feeds
+    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+    
+    # static media, for dev only
+    (r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':rbsite.settings.MEDIA_ROOT}),
+)
+
