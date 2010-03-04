f2548cdb7994ce88a22a3d295e51e1a911bf62a5
Andrew Martin <werdz@redbrick.dcu.ie>
2010-03-04 18:36:19 +0000

    Added big giant warning

--- rbsite/blog/urls.py
+++ rbsite/blog/urls.py
@@ -3,6 +3,7 @@ from models import Post
 
 
 queryset = {'queryset': Post.objects.order_by('-pub_date')}
+
 urlpatterns = patterns('',
 	url('^$', 'rbsite.blog.views.index', name="blog"),
 	url('^(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', queryset, name="post"),
