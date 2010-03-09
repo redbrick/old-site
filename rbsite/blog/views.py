3c79020fc2ef40c5327963680f57c7171d6958df
Andrew Martin <werdz@redbrick.dcu.ie>
2010-03-09 22:37:31 +0000

    Design changes

--- rbsite/blog/views.py
+++ rbsite/blog/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 # Create your views here.
 def index(request):
-	return render_to_response('base.html')
+	return render_to_response('blog/index.html')
 
 def news(request):
 	post_list = Post.objects.all()
--- rbsite/urls.py
+++ rbsite/urls.py
@@ -15,18 +15,18 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	# news posts.
-	(r'^home/', include('rbsite.blog.urls')),
+	(r'^home/?', include('rbsite.blog.urls')),
 	
-	(r'^news/', 'rbsite.blog.views.news'),
+	(r'^news/?', 'rbsite.blog.views.news'),
 	
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    (r'^admin/?', include(admin.site.urls)),
 
 	# Comments
-    (r'^comments/', include('django.contrib.comments.urls')),
+    (r'^comments/?', include('django.contrib.comments.urls')),
     
     # feeds
-    (r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+    (r'^feeds/(?P<url>.*)/?$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
     
     # static media, for dev only
     (r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root':rbsite.settings.MEDIA_ROOT}),
